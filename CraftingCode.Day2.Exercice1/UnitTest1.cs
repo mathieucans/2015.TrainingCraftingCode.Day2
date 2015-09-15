f861e76c94e54e876931a7f8a7b7458133b6906e
unknown <CANSMAT@CGNP-CANS.cihmiigre.edf.fr>
2015-09-15 08:40:52 +0200

    init

---
+++ CraftingCode.Day2.Exercice1/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CraftingCode.Day2.Exercice1
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
